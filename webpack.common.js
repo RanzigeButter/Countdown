9f73731c3f063e45fd6f842be0feb71a60d4357d
Tim Schneider <38572632+RanzigeButter@users.noreply.github.com>
2020-05-07 16:27:04 +0200

    updated webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 /*  ========================================================================
-    # Webpack Configuration - Common
+    # Webpack - Common
     ========================================================================  */
 
 /**
@@ -13,7 +13,6 @@
  * Images
  * Fonts
  * Config Common
- * Module Exports
  */
 
 /*  Dependencies
@@ -25,7 +24,6 @@ const settings = require('./webpack.settings.js');
 
 // Modules
 const path = require('path');
-const merge = require('webpack-merge');
 
 // Plugins
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -71,7 +69,7 @@ const JavaScript = () => {
 
 const Images = () => {
   return {
-    test: /\.(jpg|png|gif|svg)$/,
+    test: /\.(jpe?g|png|gif|svg|webp)$/,
     include: path.resolve(__dirname, settings.paths.src.base),
     exclude: /node_modules/,
     use: [
@@ -133,8 +131,7 @@ const common = {
   name: pkg.name,
   entry: settings.entries,
   output: {
-    path: path.resolve(__dirname, settings.paths.dist.base),
-    filename: 'js/[name].min.js'
+    path: path.resolve(__dirname, settings.paths.dist.base)
   },
   module: {
     rules: [JavaScript(), Images(), Fonts()]
@@ -145,7 +142,4 @@ const common = {
   ]
 };
 
-/*  Module Exports
-    ========================================================================  */
-
-module.exports = merge(common);
+module.exports = common;
